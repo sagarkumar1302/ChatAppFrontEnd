61b7dc3fc0cc4be3d38ffc44153d71f9b52cc008
Sagar <imsgr10@gmail.com>
2025-03-17 00:16:44 +0530

    Minor

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://chatappbackend-production-6b03.up.railway.app",
         changeOrigin: true,
         secure: false,
       },
     },
   },
+  build: {
+    outDir: "dist",
+  },
+  base: "/", // Ensures correct routing
 });
